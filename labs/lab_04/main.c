209398e0ecfd7af27a1351b23fa1f921041b7b3e
takacsjoco <takacs.jozsef@student.ms.sapientia.ro>
2025-03-25 23:54:31 +0200

    hazi

--- labs/lab_04/main.c
+++ labs/lab_04/main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
-#include "cmake-build-debug/array.h"
+#include "array.h"
 
 int main(void) {
     IntArray array;
     createIntArray(10, &array);
     printArray(array);
-    isEmpty(array);
-    isFull(array);
-    insertLast(&array, 5 );
-    insertFirst(&array, 1 );
-
-
-
-
-    deallocateIntArray(&array);
+    insertLast(&array, 4);
+    printArray(array);
+    printf("\n");
+    int i = 0;
+    while (array.size < array.capacity/2) {
+        insertLast(&array, i + 1);
+        i++;
+    }
+    printArray(array);
+    insertAt(&array, 3, 8);
+    printf("\n");
+    printArray(array);
+    deleteItemAt(&array, 3);
+    printArray(array);
+    while (array.size < array.capacity) {
+        if (i%2 != 0)
+            insertLast(&array, i + 1);
+        else
+            insertFirst(&array, i + 1);
+        i++;
+    }
     return 0;
 }
