85dfc8c624224332563569f04aa4343cd0486909
takacsjoco <takacs.jozsef@student.ms.sapientia.ro>
2025-03-26 11:38:21 +0200

    problem when compiling

--- labs/lab_05/main.c
+++ labs/lab_05/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "cmake-build-debug/stack.h"
+#include "cmake-build-debug/constants.h"
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    Stack stack;
+    createStack(10, &stack);
+    srand(time(NULL));
+    while (!isFull(stack)){
+        int randomNum = rand() % 100;
+        push(&stack, randomNum);
+    }
+
     return 0;
 }
