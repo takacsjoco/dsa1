35e96ca88cbe4e0681fa4f34be8d2bb323b5c002
takacsjoco <takacs.jozsef@student.ms.sapientia.ro>
2025-04-16 11:07:31 +0300

    lab8

---
+++ labs/lab_08/list.c
@@ -0,0 +1,43 @@
+//
+// Created by user on 16.04.2025.
+//
+
+#include "list.h"
+#include <stdlib.h>
+#include <malloc.h>
+#include <stdio.h>
+
+Node * newNode(int new_data) {
+    Node *node = (Node*)malloc(sizeof(Node));
+    if(!node) {
+        exit(-1);
+    }
+    node->data = new_data;
+    node->next = NULL;
+    return node;
+}
+
+void insertAtBeginning(Node **head_ref, int new_data) {
+    Node *new_node = newNode(new_data);
+    new_node->next = *head_ref;
+    *head_ref = new_node;
+}
+
+void insertAfter(Node *prev_ref, int new_data) {
+    Node *temp = prev_ref->next;
+    Node *new_node = newNode(new_data);
+    prev_ref->next = new_node;
+    new_node->next = temp;
+}
+
+void insertAtEnd(Node **head_ref, int new_data) {
+    
+}
+
+void printList(Node *node) {
+    while (node != NULL) {
+        printf("%d -> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
--- labs/lab_08/main.c
+++ labs/lab_08/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+#include "list.h"
+
 int main(void) {
-    printf("Hello, World!\n");
+    Node *head = newNode(3);
+    printList(head);
+
+    insertAfter(head, 5);
+    printList(head);
+
+    insertAfter(head, 6);
+    printList(head);
+
+    insertAfter(head->next, 2);
+    printList(head);
+
+    insertAtBeginning(&head, 1);
+    printList(head);
+
     return 0;
 }
