75e0a9778a889105badb6b315daf89bf06b347b5
takacsjoco <takacs.jozsef@student.ms.sapientia.ro>
2025-05-07 11:18:44 +0300

    x

--- labs/lab_09/binarySearchTree.c
+++ labs/lab_09/binarySearchTree.c
@@ -2,41 +2,4 @@
 // Created by user on 30.04.2025.
 //
 
-#include "binarySearchTree.h"
-
-#include <stdio.h>
-#include <stdlib.h>
-
-NodeTree *createNewNode(int newData) {
-    NodeTree *newItem = (NodeTree*) malloc(sizeof (NodeTree));
-
-    newItem->info = newData;
-    newItem->left = newItem->right = NULL ;
-    return newItem;
-}
-
-NodeTree *insertLeft(NodeTree *root, int newData) {
-    root->left = createNewNode(newData);
-    return root->left;
-}
-
-NodeTree *insertRight(NodeTree *root, int newData) {
-    root->right= createNewNode(newData);
-    return root->right;
-}
-
-void inorderTraversal(NodeTree *root) {
-    if(root == NULL) return;
-    inorderTraversal(root->left);
-    printf("%d ", root->info);
-    inorderTraversal(root->right);
-}
-
-void destroyBinaryTree(NodeTree **root) {
-    if ((*root) != NULL) {
-        destroyBinaryTree(&(*root)->left);
-        destroyBinaryTree(&(*root)->right);
-        free(*root);
-        *root = NULL;
-    }
-}
+in
