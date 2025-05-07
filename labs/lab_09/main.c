11a83ba37f188a33e1ebc9112c24cb2961b01ae9
takacsjoco <takacs.jozsef@student.ms.sapientia.ro>
2025-05-07 10:26:02 +0300

    teszt 05.07

--- labs/lab_09/main.c
+++ labs/lab_09/main.c
@@ -2,30 +2,6 @@
 #include "list.h"
 
     int main(void) {
-        if (!freopen("baratok.txt", "r", stdin)) {
-            printf("ERROR WHEN OPENING FILE!");
-            return -1;
-        }
-        Barat uj;
-        Node *head=NULL;
-        while ((scanf("%[^;];%d;%d;%d;%c;%d\n",
-        uj.nev,
-        &uj.szuletesiDatum.ev,
-        &uj.szuletesiDatum.honap,
-        &uj.szuletesiDatum.nap,
-        &uj.nem,
-        &uj.bulizas) != EOF))
-
-             {
-                 if(head==NULL) {
-                     head = newNode(uj);
-                 }
-                 else
-                 {
-                     insertAtEnd(&head,uj);
-                 }
-             }
-        printList(head);
-        freopen("CON", "r", stdin);
+        newNode(8);
     return 0;
 }
